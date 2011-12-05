d23177b08a10393827cfaec990d9af0b749bc16d
Sahil Shah <sahil@verticalbrands.com>
2011-12-05 13:51:53 -0800

    app initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fluffy::Application.config.secret_token = '791708ef7944495a4b4f2d507d7deb0bfea1f5fc1ddd4edbb4f24c9e5b87116320f7bba39136b5faac0c7da7ad23897c8dc6940ffb8098f8e86e78ae8f86f3f5'
